cdcfca090bb5df0a942421b7bcc13f9b33d5b170
Aryan Ganotra <aryanganotra7@gmail.com>
2021-01-09 22:44:45 +0530

    init

---
+++ desktop/src/com/mathsgame/mathomon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mathsgame.mathomon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mathsgame.mathomon.Mathomon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mathomon(), config);
+	}
+}
